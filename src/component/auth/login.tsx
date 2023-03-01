52bfaa410870fc5a73e3bd4ca2a63981e4a3b1fb
JohnnyOhms <officialjohn662@gmail.com>
2023-03-01 14:49:01 +0100

    add: login auth added

---
+++ src/component/auth/login.tsx
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import { Axios } from "../../utils/axiosInstance";
+import { useAppDispatch } from "../../hooks/hooks";
+import { loginUser } from "../../slice/authSlice";
+import { useNavigate } from "react-router";
+import Navbar from "../Navbar/Navbar";
+import SideBar from "../sideBar/SideBar";
+import { Link } from "react-router-dom";
+import Typography from "@mui/material/Typography/Typography";
+
+const Login = () => {
+  const [inputValues, setInputValue] = useState({
+    email: "",
+    password: "",
+    authMssg: "",
+  });
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { email, password } = inputValues;
+    if (!email || !password) {
+      return alert("input fields cannot be blank");
+    }
+
+    Axios.post("/auth/login", {
+      email,
+      password,
+    })
+      .then((res) => {
+        dispatch(
+          loginUser({
+            user: res.data.user.user,
+            token: res.data.user.token,
+            loading: true,
+          })
+        );
+        setInputValue((prev) => ({ ...prev, authMssg: "Acoount logged in" }));
+      })
+      .then(() => {
+        setInputValue((prev) => ({ ...prev, authMssg: "" }));
+        dispatch(loginUser({ loading: false }));
+        navigate("/");
+      })
+      .catch((err) =>
+        setInputValue((prev) => ({ ...prev, authMssg: err.response.data }))
+      );
+  };
+
+  return (
+    <>
+      <Navbar />
+      <SideBar />
+      <div className="root">
+        <div className="grid align__item">
+          <Typography sx={{ fontSize: "13px", marginBottom: "10px" }}>
+            {inputValues.authMssg ? inputValues.authMssg : null}
+          </Typography>
+          <div className="register">
+            <h2>Sign In</h2>
+
+            <form className="form" onSubmit={handleSubmit}>
+              <div className="form__field">
+                <input
+                  type="email"
+                  placeholder="email@example.com"
+                  value={inputValues.email}
+                  onChange={(e) =>
+                    setInputValue((prev) => ({
+                      ...prev,
+                      email: e.target.value,
+                    }))
+                  }
+                />
+              </div>
+
+              <div className="form__field">
+                <input
+                  type="password"
+                  placeholder="password"
+                  value={inputValues.password}
+                  onChange={(e) =>
+                    setInputValue((prev) => ({
+                      ...prev,
+                      password: e.target.value,
+                    }))
+                  }
+                />
+              </div>
+
+              <div className="form__field">
+                <input
+                  type="submit"
+                  value="Sign In"
+                  style={{ cursor: "pointer" }}
+                />
+              </div>
+            </form>
+            <Link to="/auth/register">
+              <p>Don't have an accout? Register</p>
+            </Link>
+            <Link to="/auth/forgot-password">
+              <p>Forgot password ? click Here</p>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
